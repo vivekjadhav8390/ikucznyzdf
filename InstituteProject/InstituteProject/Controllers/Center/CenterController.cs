c6902784ba5f96bb1cae5115de8ed9223e64d788
vivekjadhav8390 <VivekJadhav$8390>
2018-12-08 22:29:45 +0530

    New Project Created

---
+++ InstituteProject/InstituteProject/Controllers/Center/CenterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InstituteProject.Controllers.Center
+{
+    public class CenterController : Controller
+    {
+        // GET: Center
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CenterLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ InstituteProject/InstituteProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InstituteProject.Startup))]
+namespace InstituteProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
